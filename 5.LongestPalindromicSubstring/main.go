64d15cad27e315f3fef3b2f8d9075a0db758a2ba
pengliheng <pengliheng111@gmail.com>
2019-02-17 16:39:54 +0800

    完成-fund-median-sorted-array

--- 5.LongestPalindromicSubstring/main.go
+++ 5.LongestPalindromicSubstring/main.go
@@ -4,6 +4,11 @@ import "fmt"
 
 func longestPalindromicSubstring(s string) string {
 	fmt.Printf(s)
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s); j++ {
+			fmt.Println(i, j)
+		}
+	}
 	return s
 }
 
