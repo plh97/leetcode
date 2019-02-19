15ca1c0b9571516d2750e8b9be6855df973e5828
pengliheng <pengliheng111@gmail.com>
2019-02-19 12:38:46 +0800

    更新第六道题

--- 6.ZigZagConversion/main.go
+++ 6.ZigZagConversion/main.go
@@ -4,20 +4,25 @@ import (
 	"bytes"
 )
 
-// 有意思啊.传值还是传指针, very fun
-
-func convert(s string, numRows /*总列数*/ int) string {
+func convert(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+	if len(s) < 3 {
+		return s
+	}
 	ss := bytes.Buffer{}
-	// 第一行
 	for i := 0; i < len(s); i += (2*numRows - 2) {
 		ss.WriteByte(s[i])
 	}
-	// 中间行
-	for i := 0; i < numRows-1; i++ {
-		ss.WriteByte(s[i*(2*numRows - 2)+1])
-
+	for i := 0; i < numRows-2; i++ {
+		for j := 1; j < len(s)-i; j += (2*numRows - 2) {
+			ss.WriteByte(s[j+i])
+			if j+(2*numRows-2)-i-2 < len(s) {
+				ss.WriteByte(s[j+2*numRows-i-4])
+			}
+		}
 	}
-	// 第 2<=>n-1 行
 	for i := numRows - 1; i < len(s); i += (2*numRows - 2) {
 		ss.WriteByte(s[i])
 	}
