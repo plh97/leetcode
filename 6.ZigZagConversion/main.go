0e525488a6a0d16f9f8258a8b885e55a24c0e664
pengliheng <pengliheng111@gmail.com>
2019-02-19 10:45:06 +0800

    update-readme.md

--- 6.ZigZagConversion/main.go
+++ 6.ZigZagConversion/main.go
@@ -8,44 +8,18 @@ import (
 
 func convert(s string, numRows /*总列数*/ int) string {
 	ss := bytes.Buffer{}
-	// index := 0
-	// for i := 0; i < numRows; i++ {
-	// 	// fmt.Printf("%c %d\n", ss, (2*numRows - 2))
-	// 	if i == 0 {
-	// 		for j := 0; j < len(s)/numRows; j++ {
-	// 			ss.WriteByte(s[(2*numRows-2)*j])
-	// 			index++
-	// 		}
-
-	// 		for j := 0; j < len(s)/numRows; j++ {
-	// 			if(index==len(s)-1) {
-	// 				return ss.String()
-	// 			}
-	// 			fmt.Println(index)
-	// 			// ss.WriteByte(s[(2*numRows-2)*j + numRows-2])
-	// 			index++
-	// 		}
-	// 	}
-	// }
 	// 第一行
 	for i := 0; i < len(s); i += (2*numRows - 2) {
 		ss.WriteByte(s[i])
-		// index++
 	}
 	// 中间行
-	for i := 0; i < numRows-2; i++ {
-		ss.WriteByte(s[i])
-		// index++
+	for i := 0; i < numRows-1; i++ {
+		ss.WriteByte(s[i*(2*numRows - 2)+1])
+
 	}
 	// 第 2<=>n-1 行
 	for i := numRows - 1; i < len(s); i += (2*numRows - 2) {
 		ss.WriteByte(s[i])
-		// index++
 	}
-
 	return ss.String()
 }
-
-// func main() {
-// 	fmt.Println(convert("PAYPALISHIRING", 4))
-// }
