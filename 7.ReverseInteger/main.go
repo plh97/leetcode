369de8a702261a68ab1e4dbf6d5348fa970fc792
pengliheng <pengliheng111@gmail.com>
2019-02-21 22:28:14 +0800

    更新第七道题

--- 7.ReverseInteger/main.go
+++ 7.ReverseInteger/main.go
@@ -1,40 +1,24 @@
 package convert
 
 import (
-	"fmt"
 	"math"
-	"strconv"
-	"strings"
 )
 
 func reverse(x int) int {
-	var res int64 = 0
 	sign := 1
-	fmt.Println("before reverse: ", x)
+	res := 0
 	if x < 0 {
 		sign = -1
+		x = -1 * x
 	}
-	// if x > 0 {
-	// 	ss := strings.Split(strconv.Itoa(x), "")
-	// 	for i := (len(ss) - 1) / 2; i >= 0; i-- {
-	// 		ss[i], ss[len(ss)-1-i] = ss[len(ss)-1-i], ss[i]
-	// 	}
-	// 	res, _ = strconv.ParseInt(strings.Join(ss, ""), 10, 32)
-	// } else {
-	ss := strings.Split(strconv.Itoa(-x), "")
-	for i := (len(ss) - 1) / 2; i >= 0; i-- {
-		ss[i], ss[len(ss)-1-i] = ss[len(ss)-1-i], ss[i]
+	for x > 0 {
+		temp := x % 10
+		res = res*10 + temp
+		x = x / 10
 	}
-	res, _ = strconv.ParseInt(strings.Join(ss, ""), 10, 32)
-	_res:=int(res)
-	_res = sign * _res
-	// }
-	fmt.Println("after reverse: ", res)
-	fmt.Println(int(res) > math.MaxInt32 || int(res) < math.MinInt32)
-	fmt.Println("==============")
-	if int(res) > math.MaxInt32 || int(res) < math.MinInt32 {
-		res = 0
+	res = sign * res
+	if res > math.MaxInt32 || res < -math.MaxInt32 {
+		return 0
 	}
-	return int(res)
+	return res
 }
-
