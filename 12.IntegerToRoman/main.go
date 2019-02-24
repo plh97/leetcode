0378b95a4288cf30de4e875fc06e4bc0ea685df5
pengliheng <pengliheng111@gmail.com>
2019-02-24 20:39:11 +0800

    finish-12.IntegerToRoman

--- 12.IntegerToRoman/main.go
+++ 12.IntegerToRoman/main.go
@@ -1,30 +1,15 @@
 package intToRoman
 
-import "fmt"
-
-// import "fmt"
-// func main() {
-// 	fmt.Println(isMatch("ippi", "p*."))
-// 	// fmt.Println(isMatch("mississippi", "mis*is*ip*."))
-// }
-type Roman struct {
-	Symbol string
-	Value  int
+var RomanMap [][]string = [][]string{
+	[]string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+	[]string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+	[]string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+	[]string{"", "M", "MM", "MMM", "CD", "D", "DC", "DCC", "DCCC", "DCCCC"},
 }
 
 func intToRoman(num int) string {
-	res := ""
-	romanMap := [...]Roman{
-		{Symbol: "M", Value: 1000},
-		{Symbol: "D", Value: 500},
-		{Symbol: "C", Value: 100},
-		{Symbol: "L", Value: 50},
-		{Symbol: "X", Value: 10},
-		{Symbol: "V", Value: 5},
-		{Symbol: "I", Value: 1},
-	}
-	for i := range romanMap {
-		fmt.Println(romanMap[i])
-	}
-	return res
+	return RomanMap[3][num%10000/1000] +
+		RomanMap[2][num%1000/100] +
+		RomanMap[1][num%100/10] +
+		RomanMap[0][num%10/1]
 }
--- 12.IntegerToRoman/main_test.go
+++ 12.IntegerToRoman/main_test.go
@@ -52,11 +52,20 @@ func Test_OK(t *testing.T) {
 				str: "IX",
 			},
 		},
+
+		question{
+			p: para{
+				one: 58,
+			},
+			a: ans{
+				str: "IX",
+			},
+		},
 	}
 
 	for _, q := range qs {
 		a, p := q.a, q.p
-		ast.Equal(a.num, intToRoman(p.one), "输入:%v", p)
+		ast.Equal(a.str, intToRoman(p.one), "输入:%v", p)
 	}
 
 	// ast.Panics(func() { longestPalindrome([]int{}, []int{}) }, "对空切片求中位数，却没有panic")
