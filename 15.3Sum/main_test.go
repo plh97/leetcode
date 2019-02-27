3e934e86ef33a317791cc5ace627179ba017eb06
pengliheng <pengliheng111@gmail.com>
2019-02-27 08:36:47 +0800

    add-construct-about-hashMap

--- 15.3Sum/main_test.go
+++ 15.3Sum/main_test.go
@@ -1,4 +1,4 @@
-package threeSum
+package main
 
 import (
 	"testing"
@@ -32,7 +32,34 @@ func Test_OK(t *testing.T) {
 			a: ans{
 				one: [][]int{
 					[]int{-1, 0, 1},
-					[]int{-1, 0, 1},
+					[]int{-1, -1, 2},
+				},
+			},
+		},
+
+		question{
+			p: para{
+				one: []int{-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0},
+			},
+			a: ans{
+				one: [][]int{
+					[]int{-4, 1, 3},
+					[]int{-4, 0, 4},
+					[]int{-2, 1, 1},
+					[]int{-2, -2, 4},
+					[]int{-5, 1, 4},
+					[]int{0, 0, 0},
+				},
+			},
+		},
+
+		question{
+			p: para{
+				one: []int{0, 0, 0, 0},
+			},
+			a: ans{
+				one: [][]int{
+					[]int{0, 0, 0},
 				},
 			},
 		},
--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http")
-
-http.createServer((req,res)=>{
-  console.log(req,res);
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('mac-peng');
-}).listen("3000",()=>{
-  console.log('success listen 3000');
-})
