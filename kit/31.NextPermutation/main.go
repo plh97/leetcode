a72f6567b9e216d272822227f5ac6b68b600a0db
pengliheng <pengliheng111@gmail.com>
2019-03-23 22:37:09 +0800

    finish: 31

--- kit/31.NextPermutation/main.go
+++ kit/31.NextPermutation/main.go
@@ -2,29 +2,29 @@ package nextPermutation
 
 func nextPermutation(nums []int) {
 	var (
-		l = len(nums) -1
-		r = len(nums) -1
+			l = len(nums) -1
+			r = len(nums) -1
 	)
 	for l > 1 && nums[l] <= nums[l-1] {
-		l--
+			l--
 	}
 	if l == 0 {
-		for l < r {
-			nums[l], nums[r] = nums[r], nums[l]
-			l++
-			r--
-		}
-		return
+			for l < r {
+					nums[l], nums[r] = nums[r], nums[l]
+					l++
+					r--
+			}
+			return
 	}
 	k := r
 	for nums[k] <= nums[l-1] {
-		k--
+			k--
 	}
 	nums[k], nums[l-1] = nums[l-1], nums[k]
 	for l < r {
-		nums[l], nums[r] = nums[r], nums[l]
-		l++
-		r--
+			nums[l], nums[r] = nums[r], nums[l]
+			l++
+			r--
 	}
 	return
 }
