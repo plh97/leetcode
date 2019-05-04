df5464b42895c8e71c1f0ce53b0d7b19eb0893a8
彭立衡 <pengliheng111@gmail.com>
2019-05-05 03:38:42 +0800

    finish: 557

--- kit/557.ReverseWordsInAStringIII/main.go
+++ kit/557.ReverseWordsInAStringIII/main.go
@@ -1,6 +1,29 @@
 package reverseWords
 
+import "strings"
+
 func reverseWords(s string) string {
-	res:=""
-	return s
+	arr := strings.Split(s, " ")
+	for i := range arr {
+		arr[i] = swep(arr[i])
+	}
+	res := ""
+	for i := range arr {
+		res += arr[i]
+		if i != len(arr)-1 {
+			res += " "
+		}
+	}
+	return res
+}
+
+func swep(ss string) string {
+	l, r := 0, len(ss)-1
+	s := []rune(ss)
+	for l < r {
+		s[l], s[r] = s[r], s[l]
+		l++
+		r--
+	}
+	return string(s)
 }
