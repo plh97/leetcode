7f1843670ebabff7004eb1c87dd8f84da79627b0
彭立衡 <pengliheng111@gmail.com>
2019-05-19 20:05:19 +0800

    feat(104): improve coverage
    g

--- algorithm/108.ConvertSortedArraytoBinarySearchTree/main.go
+++ algorithm/108.ConvertSortedArraytoBinarySearchTree/main.go
@@ -14,7 +14,39 @@ type TreeNode = Helper.TreeNode
  *     Right *TreeNode
  * }
  */
+var NULL = -1 << 63
+
 func sortedArrayToBST(nums []int) *TreeNode {
 	
-	return nil
+	n := len(nums)
+	if n == 0 {
+		return nil
+	}
+
+	root := &TreeNode{
+		Val: nums[0],
+	}
+
+	queue := make([]*TreeNode, 1, n*2)
+	queue[0] = root
+
+	i := 1
+	for i < n {
+		node := queue[0]
+		queue = queue[1:]
+
+		if i < n && nums[i] != NULL {
+			node.Left = &TreeNode{Val: nums[i]}
+			queue = append(queue, node.Left)
+		}
+		i++
+
+		if i < n && nums[i] != NULL {
+			node.Right = &TreeNode{Val: nums[i]}
+			queue = append(queue, node.Right)
+		}
+		i++
+	}
+
+	return root
 }
