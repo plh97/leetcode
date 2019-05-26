d25fe60de50d078857806648b15217ba59d9373c
彭立衡 <pengliheng111@gmail.com>
2019-05-27 02:42:07 +0800

    feat(530): improve

--- algorithm/530.MinimumAbsoluteDifferenceinBST/main.go
+++ algorithm/530.MinimumAbsoluteDifferenceinBST/main.go
@@ -14,32 +14,18 @@ type TreeNode = Helper.TreeNode
  *     Right *TreeNode
  * }
  */
-func getMinimumDifference(root *TreeNode) int {
+ func getMinimumDifference(root *TreeNode) int {
 	res := 1 << 62
 	c := []int{}
 	PreOrder2TreeNode(root, &c)
 	for i := 0; i < len(c)-1; i++ {
-		if res < abs(c[i]-c[i+1]) {
-			res = abs(c[i] - c[i+1])
+		if res > c[i+1] - c[i] {
+			res = c[i+1] - c[i]
 		}
 	}
 	return res
 }
 
-func abs(a int) int {
-	if a > 0 {
-		return a
-	}
-	return -a
-}
-func min(a, b int) int {
-	if a > b {
-		return b
-	}
-	return a
-}
-
-// 先序遍历返回数组..
 func PreOrder2TreeNode(root *TreeNode, res *[]int) {
 	if root == nil {
 		return
