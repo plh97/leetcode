a8ae16008959f92aeffd97907b4f304889e1d9df
彭立衡 <pengliheng111@gmail.com>
2019-05-28 22:33:59 +0800

    fix(572): improve coverage

--- algorithm/572.SubtreeofAnotherTree/main.go
+++ algorithm/572.SubtreeofAnotherTree/main.go
@@ -19,12 +19,8 @@ func isSubtree(s *TreeNode, t *TreeNode) bool {
 	if s == nil {
 		return false
 	}
-	if s.Val == t.Val {
-		if isSameTree(s, t) {
-			return true
-		}
-	} else if s == nil {
-		return false
+	if s.Val == t.Val && isSameTree(s, t) {
+		return true
 	}
 	return isSubtree(s.Left, t) || isSubtree(s.Right, t)
 }
