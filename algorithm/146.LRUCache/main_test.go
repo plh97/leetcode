bfb73e48350393270c5d2d8001249c6200ce7161
彭立衡 <pengliheng111@gmail.com>
2019-06-02 18:08:25 +0800

    fix(146): improve coverage

--- algorithm/146.LRUCache/main_test.go
+++ algorithm/146.LRUCache/main_test.go
@@ -33,6 +33,7 @@ func Test_OK(t *testing.T) {
 	ast.Equal(-1, cache.Get(1))
 	ast.Equal(3, cache.Get(3))
 	ast.Equal(4, cache.Get(4))
+	cache.Put(4, 4) // evicts key 1
 
 	// ast.Panics(func() { longestPalindrome([]int{}, []int{}) }, "对空切片求中位数，却没有panic")
 
