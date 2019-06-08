59c86fc6859e2bf4f6d3b5f6955863f512d986b7
彭立衡 <pengliheng111@gmail.com>
2019-06-09 00:11:04 +0800

    fix(504): finish test case

--- algorithm/504.Base7/main.go
+++ algorithm/504.Base7/main.go
@@ -6,14 +6,19 @@ func convertToBase7(num int) string {
 	res := ""
 
 	if num < 0 {
-		// num *= -1
+		num *= -1
+		for num > 0 {
+			res = strconv.Itoa(num%7) + res
+			num /= 7
+		}
 		res = "-" + res
+	} else if num > 0 {
+		for num > 0 {
+			res = strconv.Itoa(num%7) + res
+			num /= 7
+		}
 	} else {
 		return "0"
-	}
-	for num > 0 {
-		res = strconv.Itoa(num%7) + res
-		num /= 7
 	}
 	return res
 }
