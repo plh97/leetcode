013835ccd0c7d76467dfbbac8ae8a234fb840812
彭立衡 <pengliheng111@gmail.com>
2019-08-31 21:35:17 +0800

    feat: improve 78

--- algorithm/78.Subsets/main.go
+++ algorithm/78.Subsets/main.go
@@ -3,14 +3,9 @@ package subsets
 func subsets(nums []int) [][]int {
 	res := [][]int{[]int{}}
 	for _, e := range nums {
-		currentOutputLength := len(res)
-		for i := 0; i < currentOutputLength; i++ {
-			tempSlice := make([]int, len(res[i]))
-			copy(tempSlice, res[i])
-			tempSlice = append(tempSlice, e)
-			res = append(res, tempSlice)
+		for _, ee := range res {
+			res = append(res, append([]int{e}, ee...))
 		}
 	}
-
 	return res
 }
