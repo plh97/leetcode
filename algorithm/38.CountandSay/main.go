e49b8d4ab827be04caa850ae3d059faf0dd8b18d
彭立衡 <pengliheng111@gmail.com>
2019-09-15 20:50:56 +0800

    feat: finish 38 83 problem

--- algorithm/38.CountandSay/main.go
+++ algorithm/38.CountandSay/main.go
@@ -1,39 +1,28 @@
 package countAndSay
 
 import (
-	"fmt"
 	"strconv"
 )
 
 func countAndSay(n int) string {
 	res := "1"
 	for n > 1 {
-		fmt.Println("=============times ============", n, res)
 		n--
 		temp := ""
-		for len(res) > 0 {
-			times := 0
-			for len(res) > 0 && res[0] == '1' {
-				times++
-				res = res[1:]
+		j := 0
+		for j < len(res) {
+
+			count := 1
+			e := res[j]
+
+			for j < len(res)-1 && res[j] == res[j+1] {
+				count++
+				j++
 			}
-			if times == 1 {
-				if len(res) == 0 {
-					temp += "11"
-					continue
-				} else {
-					temp += "1"
-				}
-			} else if times == 0 {
-				temp += "1"
-				temp += res[0:1]
-			} else if times > 1 {
-				temp += strconv.Itoa(times)
-				temp += "1"
-			}
-			if len(res) > 0 {
-				res = res[1:]
-			}
+			j++
+			temp += strconv.Itoa(count)
+			temp += string(e)
+
 		}
 		res = temp
 	}
---
+++ algorithm/83.RemoveDuplicatesfromSortedList/README.md
@@ -0,0 +1,21 @@
+# [83. Remove Duplicates from Sorted List](https://leetcode.com/problems/remove-duplicates-from-sorted-list/)
+
+## 2019/09/15
+
+### 题目 💗[medium]
+
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+```bash
+Input: 1->1->2
+Output: 1->2
+```
+
+Example 2:
+
+```bash
+Input: 1->1->2->3->3
+Output: 1->2->3
+```
